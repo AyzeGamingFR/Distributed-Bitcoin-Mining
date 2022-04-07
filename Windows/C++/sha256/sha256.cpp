8684b206874f28041fabe42652e27a0a0c6d60c6
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-07 17:31:14 +0200

    Created sha256.cpp

---
+++ Windows/C++/sha256/sha256.cpp
@@ -0,0 +1,24 @@
+namespace sha256 {
+    
+    unsigned int f1(strint datas) {
+        
+        
+        
+    };
+    unsigned int f2(string datas) {
+        
+        
+        
+    };
+    unsigned int f3(string datas) {
+        
+        
+        
+    };
+    unsigned int f4(string datas) {
+        
+        
+        
+    };
+    
+};
