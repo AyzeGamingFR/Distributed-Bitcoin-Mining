67f816b9b09150bfa6012adee638657f6dd9a542
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-07 17:39:25 +0200

    Updated sha256.cpp

--- Windows/C++/sha256/sha256.cpp
+++ Windows/C++/sha256/sha256.cpp
@@ -2,22 +2,22 @@ namespace sha256 {
     
     void f1(strint datas) {
         
-        
+        return ()
         
     };
     void f2(string datas) {
         
-        
+        return ()
         
     };
     void f3(string datas) {
         
-        
+        return ()
         
     };
     void f4(string datas) {
         
-        
+        return ()
         
     };
     
