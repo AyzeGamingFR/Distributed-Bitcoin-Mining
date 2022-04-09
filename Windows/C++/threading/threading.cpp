e422babd6b69806b6613c252d8e7a99ef0c74a95
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-09 10:05:22 +0200

    Updated threading.cpp

--- Windows/C++/threading/threading.cpp
+++ Windows/C++/threading/threading.cpp
@@ -1,5 +1,20 @@
-namespace threading() {
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+namespace threading {
     
+    string threads = {};
     
+    void createThread() {
+        
+        
+        
+    };
+    void addWork(unsigned int threadId,void work) {
+        
+        
+        
+    };
     
 };
