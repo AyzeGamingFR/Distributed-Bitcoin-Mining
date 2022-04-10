028f6723963131022d2d549564d57f377ce968fb
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-10 08:53:20 +0200

    Updated networking.cpp

--- Windows/C++/networking/networking.cpp
+++ Windows/C++/networking/networking.cpp
@@ -1,5 +1,60 @@
-namespace networking() {
+#include <socket>
+
+namespace networking {
     
+    void tcpClient,
+         tcpServer;
     
+    void startTcp(unsigned int sockets, unsigned int serverport) {
+        
+        if (sockets <= 20) {
+            
+            for (i < sockets, i += 1) {
+                
+                tcpClient[i + 1] = new socket(AF_INET, SOCK_STREAM, 0);
+                
+            }
+            for (i < sockets, i += 1) {
+                
+                tcpServer[i + 1] = new socket(AF_INET, SOCK_STREAM, 0);
+                
+            }
+            for (i < sockets, i += 1) {
+                
+                bind(tcpServer[i + 1], (sockaddr*) &addr, sizeof(addr));
+                
+            }
+            for (i < sockets, i += 1) {
+                
+                tcpServer[i + 1].listen(serverport);
+                
+            }
+            
+        } else if (sockets > 20) {
+            
+            for (i < sockets, i += 1) {
+                
+                tcpClient[i + 1] = new socket(AF_INET, SOCK_STREAM, 0);
+                
+            }
+            for (i < sockets, i += 1) {
+                
+                tcpServer[i + 1] = new socket(AF_INET, SOCK_STREAM, 0);
+                
+            }
+            for (i < sockets, i += 1) {
+                
+                bind(tcpServer[i + 1], (sockaddr*) &addr, sizeof(addr));
+                
+            }
+            for (i < sockets, i += 1) {
+                
+                tcpServer[i + 1].listen(serverport);
+                
+            }
+            
+        };
+        
+    };
     
 };
