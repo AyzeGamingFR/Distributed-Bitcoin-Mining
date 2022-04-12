b6ac88905081c24ba5d6414e311c415a92f464be
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-12 15:40:40 +0200

    Updated encryption.cpp

--- Windows/C++/encryption/encryption.cpp
+++ Windows/C++/encryption/encryption.cpp
@@ -1,1 +1,17 @@
-namespace encrypt() {};
+#include "../sha256/sha256.cpp"
+
+namespace encrypt {
+    
+    unsigned int encryptDatas(string password, unsigned int datasTurnage, string datas) {
+        
+        password = sha256(password);
+        for (i < datasTurnage, i += 1) {
+            
+            datas = atoi(stoi(datas) * stoi(password));
+            
+        }
+        return (datas)
+        
+    };
+    
+};
