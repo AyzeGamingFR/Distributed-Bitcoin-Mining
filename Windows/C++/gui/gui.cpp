ad2d878ab54433674323a1111ce990877f06c2e7
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-12 15:56:05 +0200

    Updated gui.cpp

--- Windows/C++/gui/gui.cpp
+++ Windows/C++/gui/gui.cpp
@@ -1,5 +1,32 @@
-namespace gui() {
+namespace gui {
     
+    string guis = {"texts": {}, "images": {}, "buttons": {}};
     
+    unsigned int createGUI(string name, unsigned int xSize, unsigned int ySize, unsigned int backgroundColor, unsigned int guiTransparence) {
+        
+        
+        
+    };
+    unsigned int createText(string name, unsigned int xSize, unsigned int ySize, unsigned int textColor, unsigned int textBackgroundColor, unsigned int textTransparency, unsigned int textBackgroundTransparency, string text) {
+        
+        
+        
+    };
+    unsigned int createImage(string name, unsigned int xSize, unsigned int ySize, string image) {
+        
+        
+        
+    };
+    unsigned int createButton() {
+        
+        
+        
+    };
+    
+    unsigned int updateGUI(string name, unsigned int xSize, unsigned int ySize, unsigned int backgroundColor, unsigned int guiTransparence) {
+        
+        
+        
+    };
     
 };
