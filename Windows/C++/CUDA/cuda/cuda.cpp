3b8b19c34918890bfebf97d0ed4f02b6e2755077
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-05-29 12:25:13 +0200

    Updated cuda.cpp

--- Windows/C++/CUDA/cuda/cuda.cpp
+++ Windows/C++/CUDA/cuda/cuda.cpp
@@ -1,50 +1,80 @@
 #include <string>
 
 #include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
 #include "device_launch_parameters.h"
 
-#include "sha256/sha256.cpp"
-
-__global__ void hashDatas(string* datas, unsigned int* zn, unsigned int* wtd) {
+__global__ void hashDatas(string* datas, unsigned int* zn, string* hashing_algorithm) {
     
-    if (wtd == 0) {
+    if (hashing_algorithm == "argon2") {
+        
+        string* hash;
+        for (hash[0 : zn - 1] != "0") {
+            
+            argon2.hash(datas);
+            
+        };
+        return (hash)
+        
+    } else if (hashing_algorithm == "c11") {
+        
+        string* hash;
+        return (hash)
+        
+    } else if (hashing_algorithm == "scrypt") {
+        
+        string* hash;
+        return (hash)
+        
+    } else if (hashing_algorithm == "sha256") {
+        
+        string* hash;
+        return (hash)
+        
+    } else if (hashing_algorithm == "sha512") {
+        
+        string* hash;
+        return (hash)
+        
+    } else if (hashing_algorithm == "sonoa") {
         
         
         
-    } else if (wtd == 1) {
+    } else if (hashing_algorithm == "x11") {
         
+        string* hash;
+        return (hash)
         
+    } else if (hashing_algorithm == "x12") {
         
-    } else if (wtd == 2) {
+        string* hash;
+        return (hash)
         
+    } else if (hashing_algorithm == "x16") {
         
+        string* hash;
+        return (hash)
         
-    } else if (wtd == 3) {
+    } else if (hashing_algorithm == "x16r") {
         
+        string* hash;
+        return (hash)
         
+    } else if (hashing_algorithm == "x16s") {
         
-    } else if (wtd == 4) {
-        
-        
-        
-    } else if (wtd == 5) {
-        
-        
-        
-    } else if (wtd == 6) {
-        
-        
+        string* hash;
+        return (hash)
         
     };
     
 };
 
-unsigned int cuda(string inputdatas, unsigned int zerosNeeded, unsigned int workToDo) {
+unsigned int cuda(string inputdatas, unsigned int zeros_needed) {
     
     // create the strings pointers in the gpu
     string* datas = inputdatas;
     string* hash = "0000000000000000000000000000000000000000000000000000000000000000";
-    unsigned int* zn = zerosNeeded;
+    unsigned int* zn = zeros_needed;
     
     // allocate memory in the gpu
     cudaMalloc(&datas, sizeof(datas));
@@ -57,9 +87,9 @@ unsigned int cuda(string inputdatas, unsigned int zerosNeeded, unsigned int work
     cudaMemcpy(zn, zn, sizeof(zn), cudaMemcpyHostToDevice);
     
     // run the hashing function in the gpu
-    hashDatas<<<128, sizeof(datas) / sizeof(unnsigned int)>>>(datas, zerosNeeded, wtd);
+    hashDatas<<<devProp.multiProcessorCount, sizeof(datas) / sizeof(unsigned int)>>>(datas, zerosNeeded); // hash the datas using the gpu using the amount of cuda cores of the gpu, with the size of the datas
     
     // get the work done by the gpu
-    cudaMemcpy(endingDatas, hash, sizeof(hash), cuaMemcpyDeviceToHost); // ending point of the datas, source of the datas, size
+    cudaMemcpy(endingDatas, hash, sizeof(hash), cudaMemcpyDeviceToHost); // ending point of the datas, source of the datas, size, from gpu to cpu to ram
     
 };
